40f42afd74fb5456ca468b4d459d726e4e60bc0c
Yu Steven <78414278+git-yusteven@users.noreply.github.com>
2022-03-11 15:43:04 +0800

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status_log=$(git status -sb)
 # 这里使用的是 main 分支，根据需求自行修改
-if [ "$status_log" == "## main...origin/master" ];then
+if [ "$status_log" == "## main...origin/main" ];then
   echo "nothing to commit, working tree clean"
 else
   git add .&&git commit -m "Collected by GitHub action"&&git push origin master

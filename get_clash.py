3d6272a8a004dc06d3b6b608acf2574d65eb56ee
Yu Steven <78414278+git-yusteven@users.noreply.github.com>
2022-04-01 15:20:39 +0800

    Update get_clash.py

--- get_clash.py
+++ get_clash.py
@@ -7,7 +7,7 @@ from utils.jiang import get_content as jiang_content
 changfengoss = os.path.join("changfengoss")
 dirname = time.strftime("%Y_%m_%d", time.localtime(time.time()))
 yamlUtils = YamlUtils(changfengoss)
-yamlUtils.clone_repo("https://ghproxy.com/https://github.com/changfengoss/pub.git")
+yamlUtils.clone_repo("https://github.com/changfengoss/pub.git")
 yamlUtils.make_template_dict("yaml", dirname)
 yamlUtils.save_file("pub/changfengoss.yaml")
 shutil.rmtree(changfengoss)

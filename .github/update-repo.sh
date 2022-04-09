36b1eea5e53d2bda0a89e20b4f183611067706ac
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-04-09 12:57:36 +0800

    Create update-repo.sh

---
+++ .github/update-repo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+status_log=$(git status -sb)
+# 这里使用的是 main 分支，根据需求自行修改
+if [ "$status_log" == "## main...origin/main" ];then
+  echo "nothing to commit, working tree clean"
+else
+  git add .&&git commit -m "更新节点池"&&git push origin main
+fi

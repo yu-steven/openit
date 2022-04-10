41b6edacd88fb8f24d7b8bc72303ecd85390e2cf
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-04-10 22:06:53 +0800

    Update list_merge.py

--- utils/list_merge.py
+++ utils/list_merge.py
@@ -57,7 +57,7 @@ class sub_merge():
                 print(f'Writing error of {remarks} to {ids:0>2d}.txt\n')
 
         print('Merging nodes...\n')
-        # content_all = ''.join(content_list) # https://python3-cookbook.readthedocs.io/zh_CN/latest/c02/p14_combine_and_concatenate_strings.html
+        content_all = ''.join(content_list) # https://python3-cookbook.readthedocs.io/zh_CN/latest/c02/p14_combine_and_concatenate_strings.html
         content_yaml = sub_convert.convert(content_all,'content','YAML',{'dup_rm_enabled': False, 'format_name_enabled': True})
         content_url = sub_convert.yaml_decode(content_yaml)
         content_base64 = sub_convert.base64_encode(content_url)

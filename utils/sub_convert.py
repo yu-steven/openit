7cc0f2ea653bedb57657dbb5807abfccfb781f7e
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-04-16 16:19:31 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -334,11 +334,11 @@ class sub_convert():
     
                 proxy_index = proxies_list.index(proxy)
                 if len(proxies_list) > 999:
-                    proxy['name'] = f'倾城极速.tk-{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>4d}
+                    proxy['name'] = f'{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>4d}
                 elif len(proxies_list) < 999 and len(proxies_list) > 99:
-                    proxy['name'] = f'倾城极速.tk-{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>3d}
+                    proxy['name'] = f'{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>3d}
                 elif len(proxies_list) < 99:
-                    proxy['name'] = f'倾城极速.tk-{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>2d}
+                    proxy['name'] = f'{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>2d}
 
                 if proxy['server'] != '127.0.0.1':
                     proxy_str = str(proxy)

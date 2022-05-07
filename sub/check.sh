a053fa44d77fd983f56e55233ebcffaede4f8ed6
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-05-07 20:32:34 +0800

    Update check.sh

--- sub/check.sh
+++ sub/check.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 echo -e "proxypoolcheck\n"
-echo "下载proxypoolcheck"
+echo "下载 proxypoolcheck.gz"
 wget https://github.com/Sansui233/proxypoolCheck/releases/download/v0.3.1/proxypoolCheck-linux-amd64-v0.3.1.gz -O proxypoolcheck.gz
-echo "解压proxypoolcheck"
+echo "解压 proxypoolcheck.gz"
 gzip -d proxypoolcheck.gz
-echo "chmod授予777权限"
+echo "删除 proxypoolcheck.gz"
+rm proxypoolcheck.gz
+echo "chmod 授予777权限"
 chmod 777 proxypoolcheck
 echo "静默运行程序,默认配置"
 nohup ./proxypoolcheck -c https://raw.githubusercontent.com/yu-steven/openit/main/sub/check.yaml

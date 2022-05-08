842df5ca3da6d57e7d60973125af889147b20738
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-05-08 16:38:47 +0800

    Update check.sh

--- sub/check.sh
+++ sub/check.sh
@@ -8,7 +8,7 @@ echo "unzip proxypoolcheck.gz..."
 gzip -d proxypoolcheck.gz
 echo "done"
 echo "chmod 授予777权限..."
-chmod 777 proxypoolcheck
+chmod +755 proxypoolcheck
 echo "done"
 echo "静默执行程序,默认配置..."
 nohup ./proxypoolcheck -c https://raw.githubusercontent.com/yu-steven/openit/main/sub/check.yaml

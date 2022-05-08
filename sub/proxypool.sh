70dd7e5838cfed068e97444c3da30bf6d4f6a6bd
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-05-08 16:39:03 +0800

    Update proxypool.sh

--- sub/proxypool.sh
+++ sub/proxypool.sh
@@ -5,7 +5,7 @@ echo "download proxypool..."
 wget https://github.com/daycat/proxypool/releases/download/latest/proxypool-linux-amd64 -O proxypool
 echo "done"
 echo "chmod 授予777权限..."
-chmod 777 proxypool
+chmod +777 proxypool
 echo "done"
 echo -e "静默执行程序，默认配置...\n结果会输出到12580端口"
 nohup ./proxypool -c https://raw.githubusercontent.com/yu-steven/openit/main/sub/config.yaml

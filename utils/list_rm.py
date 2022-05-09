8718593df0b67fe35d29a99a9732a971d59ef04d
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-05-09 16:02:32 +0800

    Update list_rm.py

--- utils/list_rm.py
+++ utils/list_rm.py
@@ -5,30 +5,11 @@ import json, re
 import sys, getopt
 from urllib import request
 
-def main(argv):
-   input_file = ''
-   output_file = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["input=","output="])
-   except getopt.GetoptError:
-      print ('test.py -i <input_file> -o <output_file>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print ('with -i <input_file> -o <output_file>')
-         sys.exit()
-      elif opt in ("-i", "--input"):
-         input_file = arg
-         return input_file
-      elif opt in ("-o", "--output"):
-         output_file = arg
-         return output_file
-
 # 文件路径定义
-input_file = input_file
-output_file = output_file
-
-file_list = []  #创建一个空列表
+input_file = ./url
+output_file = ./url
+#创建一个空列表
+file_list = []
 
 class sub_merge:
     def out_file(): #文件去重
@@ -57,4 +38,3 @@ class sub_merge:
 
 if __name__ =="__main__":
     sub_merge.out_file()
-    main(sys.argv[1:])

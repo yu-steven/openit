49223656bd94eecf4aadd2c680063cda5c019457
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-05-11 13:49:09 +0800

    Update check.sh

--- sub/check.sh
+++ sub/check.sh
@@ -8,4 +8,4 @@ echo "chmod 授予777权限..."
 chmod +755 proxypoolcheck
 echo "done"
 echo "静默执行程序，默认配置...结果会输出到7777端口"
-nohup ./proxypoolcheck -c https://raw.githubusercontent.com/yu-steven/openit/main/sub/check.yaml
+nohup ./proxypoolcheck -c https://raw.githubusercontent.com/yu-steven/openit/main/sub/check.yaml > /dev/null 2>&1 &

9c0d866df545f9b8a67e4829e2f39ba822de54b8
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-05-14 15:19:02 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -389,11 +389,11 @@ class sub_convert():
     
                 proxy_index = proxies_list.index(proxy)
                 if len(proxies_list) > 999:
-                    proxy['name'] = f'{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>4d}
+                    proxy['name'] = f'{name_emoji}{country_code}-{ip}-{proxy_index:0>4d}'# {name_emoji}{country_code}-{ip}-{proxy_index:0>4d}
                 elif len(proxies_list) < 999 and len(proxies_list) > 99:
-                    proxy['name'] = f'{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>3d}
+                    proxy['name'] = f'{name_emoji}{country_code}-{ip}-{proxy_index:0>3d}'# {name_emoji}{country_code}-{ip}-{proxy_index:0>3d}
                 elif len(proxies_list) < 99:
-                    proxy['name'] = f'{name_emoji}{country_code}-Openit.ml'# {name_emoji}{country_code}-{ip}-{proxy_index:0>2d}
+                    proxy['name'] = f'{name_emoji}{country_code}-{ip}-{proxy_index:0>2d}'# {name_emoji}{country_code}-{ip}-{proxy_index:0>2d}
 
                 if proxy['server'] != '127.0.0.1':
                     proxy_str = str(proxy)

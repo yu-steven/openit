31007d48d6cafb676f3c17e63b64ce25207446f8
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-05-17 09:11:59 +0800

    Update update-repo.sh

--- .github/update-repo.sh
+++ .github/update-repo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# 这里使用的是 main 分支，根据需求自行修改
 
 status_log=$(git status -sb)
-# 这里使用的是 main 分支，根据需求自行修改
+
 if [ "$status_log" == "## main...origin/main" ];then
   echo "nothing to commit, working tree clean"
 else

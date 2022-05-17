5f6363bc78ea4a45bc960056c54b2d8706cb14e0
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-05-17 13:18:43 +0800

    Update update-repo.sh

--- .github/update-repo.sh
+++ .github/update-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status_log=$(git status -s)
 # 这里使用的是 main 分支，根据需求自行修改
-if [ "$status_log" == "?? subconverter.tar.gz?? subconverter/" ];then
+if [ "$status_log" == "## main...origin/main" ];then
   echo "nothing to commit, working tree clean"
 else
   git add long&&git add Clash.yaml&&git commit -m "$(date '+%Y.%m.%d %H:%M:%S')更新订阅"&&git push origin main

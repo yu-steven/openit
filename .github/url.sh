2011841bbb435e8d0c9663e31bdcd7c8e86c0911
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-06-11 21:18:18 +0800

    Update url.sh

--- .github/url.sh
+++ .github/url.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-time=`date "+%Y.%m.%d %H:%M:%S"`
+name=Openit
 line=`sed -n '$=' url`
 url=$[line-2]
+time=`date "+%Y.%m.%d %H:%M:%S"`
 
-echo -e "REMARKS=Openit \nSTATUS=节点数量: $url.♥.更新时间: $time"
+echo -e "REMARKS=$name \nSTATUS=节点数量: $url.♥.更新时间: $time"

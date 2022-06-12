29e6a6fd8a0bc07a2bcee02ee835125dcf133587
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-06-13 00:31:42 +0800

    Update main.py

--- utils/clashcheck/main.py
+++ utils/clashcheck/main.py
@@ -14,11 +14,21 @@ import subprocess
 
 
 def check(alive, proxy, apiurl,sema,timeout):
-    r = requests.get(url=apiurl + '/proxies/'+str(proxy['name'])+'/delay?url=http://gstatic.com/generate_204&timeout='+str(timeout))
-    response = json.loads(r.text)
     try:
-        if response['delay'] > 0:
-            alive.append(proxy)
+        r = requests.get(url=apiurl + '/proxies/'+str(proxy['name'])+'/delay?url=https://gstatic.com/generate_204&timeout='+str(timeout),timeout=10)
+        response = json.loads(r.text)
+        try:
+            if response['delay'] > 0:
+                r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=https://www.youtube.com/s/player/23010b46/player_ias.vflset/en_US/remote.js&timeout=' + str(timeout), timeout=10)
+                response = json.loads(r.text)
+                try:
+                    if response['delay'] > 0:
+                        print(response['delay'])
+                        alive.append(proxy)
+                except:
+                    pass
+        except:
+            pass
     except:
         pass
 
@@ -75,7 +85,7 @@ if __name__ == '__main__':
             processes.append(p)
         for p in processes:
             p.join
-        time.sleep(10)
+        time.sleep(5)
         alive=list(alive)
         push(alive,outfile)
         shutil.rmtree('./temp')

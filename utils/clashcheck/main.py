1c31a0c2c2d1900334a9222f94fd08f931750bbc
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-06-13 19:52:48 +0800

    Update main.py

--- utils/clashcheck/main.py
+++ utils/clashcheck/main.py
@@ -14,21 +14,11 @@ import subprocess
 
 
 def check(alive, proxy, apiurl,sema,timeout):
+    r = requests.get(url=apiurl + '/proxies/'+str(proxy['name'])+'/delay?url=http://gstatic.com/generate_204&timeout='+str(timeout))
+    response = json.loads(r.text)
     try:
-        r = requests.get(url=apiurl + '/proxies/'+str(proxy['name'])+'/delay?url=https://gstatic.com/generate_204&timeout='+str(timeout),timeout=10)
-        response = json.loads(r.text)
-        try:
-            if response['delay'] > 0:
-                r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=https://www.youtube.com/s/player/23010b46/player_ias.vflset/en_US/remote.js&timeout=' + str(timeout), timeout=10)
-                response = json.loads(r.text)
-                try:
-                    if response['delay'] > 0:
-                        print(response['delay'])
-                        alive.append(proxy)
-                except:
-                    pass
-        except:
-            pass
+        if response['delay'] > 0:
+            alive.append(proxy)
     except:
         pass
 

5c5df1998b5aca5bafbe8da77a6ff5c6e1ee600c
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-06-15 11:44:05 +0800

    Create clash.py

---
+++ utils/clashcheck/clash.py
@@ -0,0 +1,50 @@
+import yaml
+import socket
+import maxminddb
+from tqdm import tqdm
+import flag
+
+def push(list, outfile):
+    country_count = {}
+    count = 1
+    clash = {'proxies': [], 'proxy-groups': [
+            {'name': 'automatic', 'type': 'url-test', 'proxies': [], 'url': 'https://www.google.com/favicon.ico',
+             'interval': 300}, {'name': 'Proxy', 'type': 'select', 'proxies': ['automatic']}],
+             'rules': ['MATCH,Proxy']}
+    with maxminddb.open_database('GeoLite2-Country.mmdb') as countrify:
+        for i in tqdm(range(int(len(list))), desc="Parse"):
+            x = list[i]
+            try:
+                float(x['password'])
+            except:
+                try:
+                    float(x['uuid'])
+                except:
+                    try:
+                        ip = str(socket.gethostbyname(x["server"]))
+                    except:
+                        ip = str(x["server"])
+                    try:
+                        country = str(countrify.get(ip)['country']['iso_code'])
+                    except:
+                        country = 'UN'
+                    if country == 'TW' or country == 'MO' or country == 'HK':
+                        flagcountry = 'CN'
+                    else:
+                        flagcountry = country
+                    try:
+                        country_count[country] = country_count[country] + 1
+                        x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(count)
+                    except:
+                        country_count[country] = 1
+                        x['name'] = str(flag.flag(flagcountry)) + " " + country + " " + str(count)
+                    clash['proxies'].append(x)
+                    clash['proxy-groups'][0]['proxies'].append(x['name'])
+                    clash['proxy-groups'][1]['proxies'].append(x['name'])
+                    count = count + 1
+            #except:
+                #print(list[i])
+                #pass
+
+    with open(outfile, 'w') as writer:
+        yaml.dump(clash, writer, sort_keys=False)

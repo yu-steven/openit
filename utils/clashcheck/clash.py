96646774c9ad9226b1cf5f30cf915d7e381d37a3
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-06-15 10:24:14 +0800

    Update clash.py

--- utils/clashcheck/clash.py
+++ utils/clashcheck/clash.py
@@ -1,8 +1,13 @@
 import yaml
 import socket
 import maxminddb
-from tqdm import tqdm
+import platform
+import os
+import requests
 import flag
+from tqdm import tqdm
+from pathlib import Path
+
 
 def push(list, outfile):
     country_count = {}
@@ -11,7 +16,7 @@ def push(list, outfile):
             {'name': 'automatic', 'type': 'url-test', 'proxies': [], 'url': 'https://www.google.com/favicon.ico',
              'interval': 300}, {'name': 'Proxy', 'type': 'select', 'proxies': ['automatic']}],
              'rules': ['MATCH,Proxy']}
-    with maxminddb.open_database('GeoLite2-Country.mmdb') as countrify:
+with maxminddb.open_database('GeoLite2-Country.mmdb') as countrify:
         for i in tqdm(range(int(len(list))), desc="Parse"):
             x = list[i]
             try:
@@ -42,9 +47,207 @@ def push(list, outfile):
                     clash['proxy-groups'][0]['proxies'].append(x['name'])
                     clash['proxy-groups'][1]['proxies'].append(x['name'])
                     count = count + 1
-            #except:
-                #print(list[i])
-                #pass
+            # except:
+            # print(list[i])
+            # pass
 
     with open(outfile, 'w') as writer:
         yaml.dump(clash, writer, sort_keys=False)
+
+
+def checkenv():
+    home = str(Path.home())
+    mmdbfl = home + '/.config/clash/Country.mmdb'
+    operating_system = str(platform.platform())
+    if operating_system.startswith('macOS'):
+        if 'arm64' in operating_system:
+            clashname = './clash-darwinarm64'
+        else:
+            clashname = './clash-darwinamd64'
+        if os.path.exists(mmdbfl):
+            os.remove(mmdbfl)
+        Countrymmdb = requests.get(
+            url='https://ghproxy.com/https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb')
+        os.makedirs('~/.config/clash/', exist_ok=True)
+        with open(mmdbfl, 'wb') as writep:
+            writep.write(Countrymmdb.content)
+
+    elif operating_system.startswith('Linux'):
+        clashname = './clash-linuxamd64'
+        if os.path.exists(mmdbfl):
+            os.remove(mmdbfl)
+        Countrymmdb = requests.get(
+            url='https://ghproxy.com/https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb')
+        os.makedirs('~/.config/clash/', exist_ok=True)
+        with open(mmdbfl, 'wb') as writep:
+            writep.write(Countrymmdb.content)
+    elif operating_system.startswith('Windows'):
+        clashname = 'clash-windowsamd64.exe'
+    else:
+        print('Unsupported Platform')
+        exit(1)
+
+    return clashname, operating_system
+
+
+def filter(config):
+    list = config["proxies"]
+    # print(list)
+    ss_supported_ciphers = ['aes-128-gcm', 'aes-192-gcm', 'aes-256-gcm',
+                            'aes-128-cfb', 'aes-192-cfb', 'aes-256-cfb', 'aes-128-ctr', 'aes-192-ctr', 'aes-256-ctr',
+                            'rc4-md5', 'chacha20-ietf', 'xchacha20', 'chacha20-ietf-poly1305',
+                            'xchacha20-ietf-poly1305']
+    ssr_supported_obfs = ['plain', 'http_simple', 'http_post', 'random_head', 'tls1.2_ticket_auth',
+                          'tls1.2_ticket_fastauth']
+    ssr_supported_protocol = ['origin', 'auth_sha1_v4', 'auth_aes128_md5', 'auth_aes128_sha1', 'auth_chain_a',
+                              'auth_chain_b']
+    vmess_supported_ciphers = ['auto', 'aes-128-gcm', 'chacha20-poly1305', 'none']
+    iplist = {}
+    passlist = []
+    count = 1
+    clash = {'proxies': [], 'proxy-groups': [
+            {'name': 'automatic', 'type': 'url-test', 'proxies': [], 'url': 'https://www.google.com/favicon.ico',
+             'interval': 300}, {'name': 'Proxy', 'type': 'select', 'proxies': ['automatic']}],
+             'rules': ['MATCH,Proxy']}
+    with maxminddb.open_database('GeoLite2-Country.mmdb') as countrify:
+        for i in tqdm(range(int(len(list))), desc="Parse"):
+            try:
+                x = list[i]
+                authentication = ''
+                try:
+                    ip = str(socket.gethostbyname(x["server"]))
+                except:
+                    ip = x['server']
+                try:
+                    country = str(countrify.get(ip)['country']['iso_code'])
+                except:
+                    country = 'UN'
+                if x['type'] == 'ss':
+                    try:
+                        if x['cipher'] not in ss_supported_ciphers:
+                            ss_omit_cipher_unsupported = ss_omit_cipher_unsupported + 1
+                            continue
+                        if country != 'CN':
+                            if ip in iplist:
+                                ss_omit_ip_dupe = ss_omit_ip_dupe + 1
+                                continue
+                            else:
+                                iplist.append(ip)
+                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SSS'
+                        authentication = 'password'
+                    except:
+                        print("shit went wrong " + x)
+                        continue
+                elif x['type'] == 'ssr':
+                    try:
+                        if x['cipher'] not in ss_supported_ciphers:
+                            continue
+                        if x['obfs'] not in ssr_supported_obfs:
+                            continue
+                        if x['protocol'] not in ssr_supported_protocol:
+                            continue
+                        if country != 'CN':
+                            if ip in iplist:
+                                continue
+                            else:
+                                iplist.append(ip)
+                        authentication = 'password'
+                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SSR'
+                    except:
+                        continue
+                elif x['type'] == 'vmess':
+                    try:
+                        if 'udp' in x:
+                            if x['udp'] not in [False, True]:
+                                continue
+                        if 'tls' in x:
+                            if x['tls'] not in [False, True]:
+                                continue
+                        if 'skip-cert-verify' in x:
+                            if x['skip-cert-verify'] not in [False, True]:
+                                continue
+                        if 'cipher' not in vmess_supported_ciphers:
+                            continue
+                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'VMS'
+                        authentication = 'uuid'
+                    except:
+                        print(x)
+                        continue
+                elif x['type'] == 'trojan':
+                    try:
+                        if 'udp' in x:
+                            if x['udp'] not in [False, True]:
+                                continue
+                        if 'skip-cert-verify' in x:
+                            if x['skip-cert-verify'] not in [False, True]:
+                                continue
+                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'TJN'
+                        authentication = 'password'
+                    except:
+                        continue
+                elif x['type'] == 'snell':
+                    try:
+                        if 'udp' in x:
+                            if x['udp'] not in [False, True]:
+                                continue
+                        if 'skip-cert-verify' in x:
+                            if x['skip-cert-verify'] not in [False, True]:
+                                continue
+                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SNL'
+                        authentication = 'psk'
+                    except:
+                        continue
+                elif x['type'] == 'http':
+                    try:
+                        if 'tls' in x:
+                            if x['tls'] not in [False, True]:
+                                continue
+                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'HTT'
+                        #authentication = 'userpass'
+                    except:
+                        continue
+                elif x['type'] == 'socks5':
+                    try:
+                        if 'tls' in x:
+                            if x['tls'] not in [False, True]:
+                                continue
+                        if 'udp' in x:
+                            if x['udp'] not in [False, True]:
+                                continue
+                        if 'skip-cert-verify' in x:
+                            if x['skip-cert-verify'] not in [False, True]:
+                                continue
+                        x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SK5'
+                        #authentication = 'userpass'
+                    except:
+                        continue
+                else:
+                    print(x)
+                    print('unsupported')
+                    continue
+                if ip in iplist and x['port'] in iplist[ip]:
+                    if country != 'CN':
+                        continue
+                    else:
+                        if x[authentication] in passlist:
+                            continue
+                        else:
+                            passlist.append(x[authentication])
+                            pass
+                else:
+                    try:
+                        iplist[ip].append(x['port'])
+                    except:
+                        iplist[ip] = []
+                        iplist[ip].append(x['port'])
+
+                clash['proxies'].append(x)
+                clash['proxy-groups'][0]['proxies'].append(x['name'])
+                clash['proxy-groups'][1]['proxies'].append(x['name'])
+                count = count + 1
+
+            except:
+                print('shitwentwrong')
+                continue
+
+    return clash

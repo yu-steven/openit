0ecd2f7381059ade6a61e714e2089340924e7011
2823027009 <2823027009@qq.com>
2022-06-16 02:05:34 +0800

    希望人有事儿
    更新rm，index已作出修改。
    什么hd，关爷的permission
    
    Co-Authored-By: Yu Steven <78414278+yu-steven@users.noreply.github.com>

--- utils/rm/index.js
+++ utils/rm/index.js
@@ -1,63 +1,109 @@
-const fs = require('fs');
+const fs = require('fs')
+const location = require('./location')
+const config = require('./config')
+
 let urls = fs.readFileSync('./rm2','utf8');
+let flags = JSON.parse(fs.readFileSync('./flags.json','utf8'))
 
 let urlList = urls.split('\n');
 let resList = [];
 let stringList = [];
 let finalList = [];
 let finalURLs = [];
+let countryList = ['unknown'];
+let emojiList =[''];
+let countryCount = {unknown:0};
+let urlCountryList = {unknown:[]}
 
-for(let i=0;i<urlList.length;i++){
-    let url = urlList[i];
-    switch (url.split('://')[0]) {
-        case 'vmess':
-            let vmessJSON = JSON.parse(Buffer.from(url.split('://')[1], 'base64').toString('utf-8'));
-            vmessJSON.ps = null
-            resList.push({type: 'vmess', data: vmessJSON});
-            break
-        case 'trojan':
-            let trojanData = url.split('://')[1];
-            resList.push({type: 'trojan', data: trojanData.split('#')[0]})
-            break
-        case 'ss':
-            let ssData = url.split('://')[1];
-            resList.push({type: 'ss', data: ssData.split('#')[0]})
-            break
-        case 'ssr':
-            let ssrData = Buffer.from(url.split('://')[1], 'base64').toString('utf-8');
-            resList.push({type: 'ssr', data:ssrData.replace(/remarks=\w+?&/,'remarks={name}&')});
-            break
-        default:
-            break
+async function run(){
+    //处理flags
+    for(let i=0;i<flags.length;i++){
+        countryList.push(flags[i].code);
+        emojiList.push(flags[i].emoji);
+        countryCount[flags[i].code] = 0;
+        urlCountryList[flags[i].code] = [];
     }
-}
-for(let i=0;i<resList.length;i++){
-    stringList.push(JSON.stringify(resList[i]))
-}
-let afterList = Array.from(new Set(stringList))
-for(let i=0;i<afterList.length;i++){
-    finalList.push(JSON.parse(afterList[i]))
+
+    //解析URL
+    for(let i=0;i<urlList.length;i++){
+        let url = urlList[i];
+        switch (url.split('://')[0]) {
+            case 'vmess':
+                let vmessJSON = JSON.parse(Buffer.from(url.split('://')[1], 'base64').toString('utf-8'));
+                vmessJSON.ps = null
+                resList.push({type: 'vmess', data: vmessJSON,address:vmessJSON.add});
+                break
+            case 'trojan':
+                let trojanData = url.split('://')[1];
+                let trojanAddress = trojanData.split('@')[1].split('?')[0].split(':')[0];
+                resList.push({type: 'trojan', data: trojanData.split('#')[0],address:trojanAddress})
+                break
+            case 'ss':
+                let ssData = url.split('://')[1];
+                let ssAddress = ssData.split('@')[1].split('#')[0].split(':')[0];
+                resList.push({type: 'ss', data: ssData.split('#')[0],address:ssAddress})
+                break
+            case 'ssr':
+                let ssrData = Buffer.from(url.split('://')[1], 'base64').toString('utf-8');
+                let ssrAddress = ssrData.split(':')[0];
+                resList.push({type: 'ssr', data:ssrData.replace(/remarks=\w+?&/,'remarks={name}&'),address:ssrAddress});
+                break
+            default:
+                break
+        }
+    }
+
+    //去重时要先将对象转为字符串
+    for(let i=0;i<resList.length;i++){
+        stringList.push(JSON.stringify(resList[i]))
+    }
+
+    //去重
+    let afterList = Array.from(new Set(stringList))
+
+    //转回来
+    for(let i=0;i<afterList.length;i++){
+        finalList.push(JSON.parse(afterList[i]))
+    }
+
+    //批量测试国家
+    for(let i=0;i<finalList.length;i++){
+        finalList[i].country = await location.get(finalList[i].address)
+    }
+
+    //变回链接
+    for(let i=0;i<finalList.length;i++){
+        let item = finalList[i];
+        countryCount[finalList[i].country]++
+        let name = emojiList[countryList.indexOf(finalList[i].country)]+finalList[i].country+' '+countryCount[finalList[i].country]+config.nodeAddName
+        switch (item.type){
+            case 'vmess':
+                item.data.ps = (name).toString();
+                urlCountryList[finalList[i].country].push('vmess://'+Buffer.from(JSON.stringify(item.data),'utf8').toString('base64'));
+                break
+            case 'trojan':
+                urlCountryList[finalList[i].country].push('trojan://'+item.data+'#'+(name).toString())
+                break
+            case 'ss':
+                urlCountryList[finalList[i].country].push('ss://'+item.data+'#'+(name).toString())
+                break
+            case 'ssr':
+                urlCountryList[finalList[i].country].push('ssr://'+Buffer.from(item.data.replace('{name}',Buffer.from((name).toString(),'utf8').toString('base64')),'utf8').toString('base64'));
+                break
+            default:
+                break
+        }
+    }
+    for(const i in urlCountryList){
+        if(urlCountryList[i].length === 0 ){
+        }else{
+            for (let a=0;a<urlCountryList[i].length;a++){
+                finalURLs.push(urlCountryList[i][a])
+            }
+        }
+    }
+    console.log(`去重完成，总共${urlList.length}个节点，去重${urlList.length-finalURLs.length}个节点，剩余${finalURLs.length}个节点。`)
+    fs.writeFileSync('./url',finalURLs.join('\n'))
 }
 
-for(let i=0;i<finalList.length;i++){
-    let item = finalList[i];
-    switch (item.type){
-        case 'vmess':
-            item.data.ps = (i+1).toString();
-            finalURLs.push('vmess://'+Buffer.from(JSON.stringify(item.data),'utf8').toString('base64'));
-            break
-        case 'trojan':
-            finalURLs.push('trojan://'+item.data+'#'+(i+1).toString())
-            break
-        case 'ss':
-            finalURLs.push('ss://'+item.data+'#'+(i+1).toString())
-            break
-        case 'ssr':
-            finalURLs.push('ssr://'+Buffer.from(item.data.replace('{name}',Buffer.from((i+1).toString(),'utf8').toString('base64')),'utf8').toString('base64'));
-            break
-        default:
-            break
-    }
-}
-console.log(`去重完成 \n总共${urlList.length}个节点，去重${urlList.length-finalURLs.length}个节点，剩余${finalURLs.length}个节点。`)
-fs.writeFileSync('./url',finalURLs.join('\n'))
+run()
---
+++ utils/rm/node_modules/ip-address/lib/v6/attributes.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var common = require('../common.js');
+var v6 = require('./constants.js');
+
+/**
+ * Returns true if the address is valid, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.isValid = function () {
+  return this.valid;
+};
+
+/**
+ * Returns true if the given address is in the subnet of the current address
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.isInSubnet = common.isInSubnet;
+
+/**
+ * Returns true if the address is correct, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.isCorrect = common.isCorrect(v6.BITS);
+
+/**
+ * Returns true if the address is in the canonical form, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.isCanonical = common.falseIfInvalid(function () {
+  return this.addressMinusSuffix === this.canonicalForm();
+});
+
+/**
+ * Returns true if the address is a link local address, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.isLinkLocal = common.falseIfInvalid(function () {
+  // Zeroes are required, i.e. we can't check isInSubnet with 'fe80::/10'
+  if (this.getBitsBase2(0, 64) ===
+    '1111111010000000000000000000000000000000000000000000000000000000') {
+    return true;
+  }
+
+  return false;
+});
+
+/**
+ * Returns true if the address is a multicast address, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.isMulticast = common.falseIfInvalid(function () {
+  return this.getType() === 'Multicast';
+});
+
+/**
+ * Returns true if the address is a v4-in-v6 address, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.is4 = common.falseIfInvalid(function () {
+  return this.v4;
+});
+
+/**
+ * Returns true if the address is a Teredo address, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.isTeredo = common.falseIfInvalid(function () {
+  return this.isInSubnet(new this.constructor('2001::/32'));
+});
+
+/**
+ * Returns true if the address is a 6to4 address, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.is6to4 = common.falseIfInvalid(function () {
+  return this.isInSubnet(new this.constructor('2002::/16'));
+});
+
+/**
+ * Returns true if the address is a loopback address, false otherwise
+ * @memberof Address6
+ * @instance
+ * @returns {boolean}
+ */
+exports.isLoopback = common.falseIfInvalid(function () {
+  return this.getType() === 'Loopback';
+});

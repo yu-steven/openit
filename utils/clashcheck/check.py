b64d3e3b1d7c057873425dd44c43f83f72e3522f
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-07-01 11:14:24 +0800

    Update check.py

--- utils/clashcheck/check.py
+++ utils/clashcheck/check.py
@@ -13,7 +13,7 @@ def check(alive, proxy, apiurl,sema,timeout, testurl):
                 response = json.loads(r.text)
                 try:
                     if response['delay'] > 0:
-                        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=https://cachefly.cachefly.net/1mb.test&timeout=' + str(timeout), timeout=15)
+                        r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url=https://cachefly.cachefly.net/10mb.test&timeout=' + str(timeout), timeout=15)
                         response = json.loads(r.text)
                         try:
                             if response['delay'] > 0:

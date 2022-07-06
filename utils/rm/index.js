be48b6fce53fb52700cd67fd7261c998ec6a4444
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-07-06 08:21:48 +0800

    Update index.js

--- utils/rm/index.js
+++ utils/rm/index.js
@@ -131,7 +131,7 @@ async function run(){
             }
         }
     }
-    console.log(`去重完成\n总共${urlList.length}个节点，去重${urlList.length-finalURLs.length}个节点，剩余${finalURLs.length}个节点。`)
+    console.log(`去重完成\n一共${urlList.length}个节点，去重${urlList.length-finalURLs.length}个节点，剩余${finalURLs.length}个节点。`)
     fs.writeFileSync('./out',finalURLs.join('\n'))
 }
 

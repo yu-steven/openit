5087498ccceaac2f0d61690d82d2589e720118ac
daycat <iyasmalan@gmail.com>
2022-07-10 22:24:41 +0800

    Update main.py

--- utils/pool/main.py
+++ utils/pool/main.py
@@ -7,10 +7,12 @@ import time
 import yaml
 import requests
 
+headers = {'User-Agent': 'ClashforWindows/0.19.21', 'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Connection': 'keep-alive'}
+
 def fetch(proxy_list, filename):
     current_date = time.strftime("%Y_%m_%d", time.localtime())
     baseurl = 'https://raw.githubusercontent.com/changfengoss/pub/main/data/'
-    working = yaml.safe_load(requests.get(url=baseurl + current_date + '/' + filename, ).text)
+    working = yaml.safe_load(requests.get(url=baseurl + current_date + '/' + filename, timeout=10).text)
     data_out = []
     for x in working['proxies']:
         data_out.append(x)
@@ -19,12 +21,12 @@ def fetch(proxy_list, filename):
 def url(proxy_list, link):
     try:
         data_out=[]
-        working = yaml.safe_load(requests.get(url=link).text)
+        working = yaml.safe_load(requests.get(url=link,timeout=10, headers=headers).text)
         for x in working['proxies']:
             data_out.append(x)
         proxy_list.append(data_out)
     except:
-        print("Error in collecting "+ link )
+        print("Error in fetching "+ link )
 
 proxy_list=[]
 if __name__ == '__main__':
@@ -35,19 +37,27 @@ if __name__ == '__main__':
         with open('config.yaml', 'r') as reader:
             config = yaml.load(reader,Loader=SafeLoader)
         subscribe_links = config['sub']
-        directories, total = get_file_list()
-        data = parse(directories)
-        try:
-            sfiles = len(subscribe_links)
-            tfiles = len(data[current_date]) + len(subscribe_links)
-            processes=[]
+        if config['sources']['changfeng'] == True:
+            directories, total = get_file_list()
+            data = parse(directories)
+            print(str(len(data[current_date])) + " clash configs found in ChangfengOSS")
+            processes = []
             filenames = list()
             filenames = data[current_date]
-        except KeyError:
-            print("Success: " + str(sfiles) + " Clash files")
-        else:
-            print("Success: " + str(tfiles) + " Clash files")
-
+            processes = []
+            try:
+                start = time.time()
+                for i in filenames:
+                    p = Process(target=fetch, args=(proxy_list, i))
+                    p.start()
+                    processes.append(p)
+                for p in processes:
+                    p.join()
+                end = time.time()
+                print("Cfoss crawl succeeded in " + "{:.2f}".format(end - start) + " seconds")
+            except:
+                end = time.time()
+                print("Cfoss crawl failed in " + "{:.2f}".format(end - start) + " seconds")
         processes=[]
         try:
             start = time.time()
@@ -57,17 +67,11 @@ if __name__ == '__main__':
                 processes.append(p)
             for p in processes:
                 p.join()
-            # for i in filenames:
-            #     p = Process(target=fetch, args=(proxy_list, i))
-            #     p.start()
-            #     processes.append(p)
-            # for p in processes:
-            #     p.join()
             end = time.time()
-            print("Collecting in " + "{:.2f}".format(end-start) + " seconds")
+            print("Subscription crawl succeeded in " + "{:.2f}".format(end-start) + " seconds")
         except:
             end = time.time()
-            print("Collecting in " + "{:.2f}".format(end-start) + " seconds")
+            print("Subscription crawl failed in " + "{:.2f}".format(end-start) + " seconds")
 
         proxy_list=list(proxy_list)
         proxies = makeclash(proxy_list)

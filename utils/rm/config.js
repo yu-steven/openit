5645fc88b8192fb9f81b551d5d15e6e5f95fc486
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-07-19 18:22:06 +0800

    Update config.js

--- utils/rm/config.js
+++ utils/rm/config.js
@@ -1,5 +1,5 @@
 module.exports={
-    nodeAddName:' → https://t.me/xingchen0829',
+    nodeAddName:' → https://t.me/SCINV',
     dnsServers:['8.8.8.8','1.1.1.1'],
     enableMediaUnlockTest:false
 }

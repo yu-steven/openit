79aec528f5e46106da669ee9b4ccbf2cb1c5cc4e
Yu Steven <78414278+yu-steven@users.noreply.github.com>
2022-07-19 19:02:36 +0800

    Delete utils/rm directory

--- utils/rm/config.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    nodeAddName:' → https://t.me/s/SCINV',
-    dnsServers:['8.8.8.8','1.1.1.1'],
-    enableMediaUnlockTest:false
-}
--- utils/rm/node_modules/ip-address/ip-address-globals.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var ip = require('./ip-address.js');
-
-window.Address4 = ip.Address4;
-window.Address6 = ip.Address6;
-
-window.v6 = ip.v6;

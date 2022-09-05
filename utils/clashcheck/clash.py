f3c22b59779a54730b3161e4430127d154b978ef
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-09-05 14:55:19 +0800

    Update clash.py

--- utils/clashcheck/clash.py
+++ utils/clashcheck/clash.py
@@ -55,6 +55,26 @@ def push(list, outfile):
         yaml.dump(clash, writer, sort_keys=False)
 
 
+def checkenv():
+    home = str(Path.home())
+    mmdbfl = home + '/.config/clash/Country.mmdb'
+    operating_system = str(platform.platform())
+    if operating_system.startswith('macOS'):
+        if 'arm64' in operating_system:
+            clashname='./clash-darwinarm64'
+        else:
+            clashname='./clash-darwinamd64'
+    elif operating_system.startswith('Linux'):
+        clashname='./clash-linuxamd64'
+    elif operating_system.startswith('Windows'):
+        clashname='clash-windowsamd64.exe'
+    else:
+        print('Unsupported Platform')
+        exit(1)
+    print('Running on '+ operating_system)
+
+    return clashname, operating_system
+
 def filter(config):
     list = config["proxies"]
     # print(list)

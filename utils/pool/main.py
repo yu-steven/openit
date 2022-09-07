6a5b7a285ccb19a19c1fd9b8ae1b7949c42b9253
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-09-07 15:48:20 +0800

    Update main.py

--- utils/pool/main.py
+++ utils/pool/main.py
@@ -12,7 +12,7 @@ headers = {'Accept': '*/*', 'Accept-Encoding': 'gzip', 'Connection': 'Keep-Alive
 def fetch(proxy_list, filename):
     current_date = time.strftime("%Y_%m_%d", time.localtime())
     baseurl = 'https://raw.githubusercontent.com/changfengoss/pub/main/data/'
-    working = yaml.safe_load(requests.get(url=baseurl + current_date + '/' + filename, timeout=120).text)
+    working = yaml.safe_load(requests.get(url=baseurl + current_date + '/' + filename, timeout=240).text)
     data_out = []
     for x in working['proxies']:
         data_out.append(x)
@@ -21,7 +21,7 @@ def fetch(proxy_list, filename):
 def url(proxy_list, link):
     try:
         data_out=[]
-        working = yaml.safe_load(requests.get(url=link, timeout=120, headers=headers).text)
+        working = yaml.safe_load(requests.get(url=link, timeout=240, headers=headers).text)
         for x in working['proxies']:
             data_out.append(x)
         proxy_list.append(data_out)

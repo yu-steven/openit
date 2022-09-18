c466d78a0899bcda7363f5921ced0b2dd871610a
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-09-18 10:16:19 +0800

    Update url.sh

--- .github/url.sh
+++ .github/url.sh
@@ -3,5 +3,5 @@ name=Openit
 line=`wc -l < url`
 time=`date '+%Y.%m.%d %H:%M:%S'`
 
-echo "$time >>> $line" >> log
+echo "$time >>> $line" >> .github/log
 echo -e "REMARKS=$name \nSTATUS=节点数量: $line.♥.更新时间: $time"

7c5db1e314b905f6579723f30e9eaeefdd32bbef
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-09-23 15:44:09 +0800

    Update index.js

--- utils/rm/index.js
+++ utils/rm/index.js
@@ -100,7 +100,7 @@ async function run(){
                 break
             case 'ssr':
                 try{
-                urlCountryList[finalList[i].country].push('ssr://'+Buffer.from(item.data.replace('{name}',Buffer.from((name).toString(),'utf8').toString('base64')),'utf8').toString('base64'))
+                urlCountryList[finalList[i].country].push('ssr://'+Buffer.from(item.data.replace('{name}',Buffer.from((name).toString(),'utf8').toString('base64').replace(/+/, "-")),'utf8').toString('base64'))
                 }catch(e){console.log('ssr node err')}
                 break
             case 'https':

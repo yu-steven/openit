88663f2ef7c963b72889635aedeb0000bab8c545
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-10-02 10:53:42 +0800

    Update main.py

--- utils/pool/main.py
+++ utils/pool/main.py
@@ -47,9 +47,9 @@ if __name__ == '__main__':
             filenames = list()
             filenames = data[current_date]
         except KeyError:
-            print("Success: " + str(sfiles) + " Clash files")
+            print("Success: " + "find" + str(sfiles) + " Clash link")
         else:
-            print("Success: " + str(tfiles) + " Clash files")
+            print("Success: " + "find" + str(tfiles) + " Clash link")
 
         processes=[]
 

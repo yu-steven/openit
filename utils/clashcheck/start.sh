dbb906a1af64d141bb56310064a2dbcc93d5bb69
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-10-10 09:03:58 +0800

    Update start.sh

--- utils/clashcheck/start.sh
+++ utils/clashcheck/start.sh
@@ -5,8 +5,14 @@
 #nohup ./start.sh &  #nohup ./start.sh > /dev/null 2>&1 &
 #TODO start clashcheck with while
 
+PID_NAME=clash
+PID_NUM=`ps -ef |grep -w $PID_NAME|grep -v grep|wc -l`
+
 while true
 do
-  pkill -9 clash-
-  /usr/bin/timeout -k 20 4m /usr/bin/python main.py
+    if [ $PID_NUM -le 0 ];then
+       /usr/bin/timeout -k 20 4m /usr/bin/python main.py
+    else
+       pkill -9 clash-
+    fi
 done

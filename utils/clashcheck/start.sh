c036d1951dc97444b46ab688009fba0adee7c565
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-10-15 23:43:38 +0800

    Update start.sh

--- utils/clashcheck/start.sh
+++ utils/clashcheck/start.sh
@@ -5,8 +5,9 @@
 #nohup ./start.sh &  #nohup ./start.sh > /dev/null 2>&1 &
 #TODO start clashcheck with while
 
+nginx
 while true
 do
   pkill -9 clash-
-  /usr/bin/timeout -k 20 4m /usr/bin/python main.py
+  timeout -k 20 4m python main.py
 done

550ddcba2df871c599b952d5c5bb75d12554dfd8
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-10-20 00:12:28 +0800

    Update url.sh

--- .github/url.sh
+++ .github/url.sh
@@ -4,4 +4,4 @@ line=`wc -l < url`
 time=`date '+%Y.%m.%d %H:%M:%S'`
 
 echo "$time >>> $line" >> .github/log && sed -i '2d' .github/log
-echo -e "REMARKS=$name \nSTATUS=节点数量: $line.♥.更新时间: $time"
+echo -e "REMARKS=$name\nSTATUS=节点数量: $line.♥.更新时间: $time"

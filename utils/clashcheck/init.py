7c5fe8432026727739ac05d870b575cf1389dd87
Yu Steven’s mac <yxyusteven@gmail.com>
2022-10-23 21:17:27 +0800

    ok

--- utils/clashcheck/init.py
+++ utils/clashcheck/init.py
@@ -10,7 +10,7 @@ def init():
         os.mkdir('temp')
 
     config = 'data/config.yaml'
-    # read from config fil
+    # read from config file
     with open(config, 'r') as reader:
         config = yaml.load(reader, Loader=SafeLoader)
         http_port = config['http-port']

b5ec2dd3df1befcd0d94065dd7b5bd0f0bd46ea1
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-10-23 23:48:55 +0800

    Update start.sh

--- utils/clashcheck/start.sh
+++ utils/clashcheck/start.sh
@@ -4,6 +4,7 @@
 #with log ↓          #no log ↓ 
 #nohup ./start.sh &  #nohup ./start.sh > /dev/null 2>&1 &
 #TODO start clashcheck with while
+#macOS -> `zsh: command not found: timeout` -> https://stackoverflow.com/questions/3504945/timeout-command-on-mac-os-x
 
 nginx
 while true; do timeout -k 20 4m python main.py; done

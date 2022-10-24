f9bf4ab38973adaff04823a04b9d7fd92359924a
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-10-24 17:15:02 +0800

    Update webhook.sh

--- .github/webhook.sh
+++ .github/webhook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST https://api.github.com/repos/yu-steven/openit/dispatches -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token ${{ secrets.webhook }}" --data '{"event_type": "Webhook"}'
+curl -X POST https://api.github.com/repos/yu-steven/openit/dispatches -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $1" --data '{"event_type": "Webhook"}'

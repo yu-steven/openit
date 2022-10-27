569689aabb728ab70aec8b5422bb7bcc0ca6607b
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-10-27 17:03:39 +0800

    Update update-repo.sh

--- .github/update-repo.sh
+++ .github/update-repo.sh
@@ -4,7 +4,7 @@ status_log=$(git status -sb)
 if [ "$status_log" == "## main...origin/main" ];then
   echo -e "\033[32mnothing to commit, working tree clean.\033[0m"
 else
-  git status -s && git add . && git commit -m "$(date '+%Y.%m.%d %H:%M:%S') 订阅更新" && git pull origin main:main && git push origin main:main
+  git status -s && git add . && git commit -m "$(date '+%Y.%m.%d %H:%M:%S') 订阅更新" && git pull origin main && git push origin main
   if [ $? == 1 ];then
     echo -e "\033[31mAutomatic merge failed; fix conflicts and then commit the result.\033[0m"
   fi

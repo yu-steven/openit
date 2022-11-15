02eb1ce98c550920a4fb2f7e9b833dd1e80abe44
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-11-15 13:58:44 +0800

    Update start.sh

--- utils/clashcheck/start.sh
+++ utils/clashcheck/start.sh
@@ -4,7 +4,6 @@
 #with log ↓          #no log ↓ 
 #nohup ./start.sh &  #nohup ./start.sh > /dev/null 2>&1 &
 #TODO start apache if exist & start clashcheck with while if no false
-#macOS -> `zsh: command not found: timeout` -> https://stackoverflow.com/questions/3504945/timeout-command-on-mac-os-x
-#                         | --------- ↑
-apachectl start || true # ↓
-while [ $? -eq 0 ]; do timeout -k 20 4m python main.py; done
+
+apachectl start || true
+while [ $? -eq 0 ]; do python main.py; done

29e405768ae1c788c030ddb1210d9ae7e2774a1e
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-11-19 18:06:38 +0800

    Update start.sh

--- utils/clashcheck/start.sh
+++ utils/clashcheck/start.sh
@@ -3,7 +3,7 @@
 #pwd /home/$USER/clashcheck
 #with log ↓          #no log ↓ 
 #nohup ./start.sh &  #nohup ./start.sh > /dev/null 2>&1 &
-#TODO start apache if docker & start clashcheck with while if no false
+#TODO start apache if in docker & start clashcheck with while if no false
 
 if [ "$in_docker" = "true" ]; then apachectl start; else echo "Hello"; fi
 while [ $? -eq 0 ]; do python main.py; done

61143abfdb530ce21a0ceb989dd8075844c854c8
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-11-20 18:36:26 +0800

    Update url.sh

--- .github/url.sh
+++ .github/url.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 name=Openitsub
-#line=$(expr $(wc -l < url) - 1)
-line=$(wc -l < url)
+line=$(expr $(wc -l < url) + 1)
 time=$(date '+%Y.%m.%d %H:%M:%S')
 
 echo "$time >>> $line" >> .github/log && sed -i '2d' .github/log

8e3db4e627e5a8abc815af935e20e8754e0f9716
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-11-21 21:58:02 +0800

    Update clash.py

--- utils/pool/clash.py
+++ utils/pool/clash.py
@@ -39,13 +39,13 @@ def push(list):
                         if x['cipher'] not in ss_supported_ciphers:
                             ss_omit_cipher_unsupported = ss_omit_cipher_unsupported + 1
                             continue
-                        if country != 'CN':
-                            if ip in iplist:
-                                ss_omit_ip_dupe = ss_omit_ip_dupe + 1
-                                continue
-                            else:
-                                iplist[ip] = []
-                                iplist[ip].append(x['port'])
+                        #if country != 'CN':
+                        #    if ip in iplist:
+                        #        ss_omit_ip_dupe = ss_omit_ip_dupe + 1
+                        #        continue
+                        #    else:
+                        #        iplist[ip] = []
+                        #        iplist[ip].append(x['port'])
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SSS'
                         authentication = 'password'
                     except:
@@ -58,12 +58,12 @@ def push(list):
                             continue
                         if x['protocol'] not in ssr_supported_protocol:
                             continue
-                        if country != 'CN':
-                            if ip in iplist:
-                                continue
-                            else:
-                                iplist.append(ip)
-                                iplist[ip].append(x['port'])
+                        #if country != 'CN':
+                        #    if ip in iplist:
+                        #        continue
+                        #    else:
+                        #        iplist.append(ip)
+                        #        iplist[ip].append(x['port'])
                         authentication = 'password'
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SSR'
                     except:

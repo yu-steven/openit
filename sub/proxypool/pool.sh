e0e2b98aef94906bd51778dc77a31c741c3edb29
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-11-21 10:38:17 +0800

    Update pool.sh

--- sub/proxypool/pool.sh
+++ sub/proxypool/pool.sh
@@ -8,7 +8,7 @@ echo "chmod 授予777权限..."
 chmod +777 proxypool
 echo "done"
 echo "静默执行程序，默认配置..."
-nohup ./proxypool -c https://raw.githubusercontent.com/yu-steven/openit/main/sub/config.yaml > /dev/null 2>&1 &
+nohup ./proxypool -c https://raw.githubusercontent.com/yu-steven/openit/main/sub/proxypool/config.yaml > /dev/null 2>&1 &
 echo "done"
 ps -e | grep proxypool
 echo "这个进程的PID是..."

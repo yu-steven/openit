0c048afbca231900607742bd1895a06facdd7219
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-11-28 11:14:35 +0800

    Update start.sh

--- utils/clashcheck/start.sh
+++ utils/clashcheck/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #python 3.x
-#pwd /home/$USER/clashcheck
+#pwd /path/to/openit/clashcheck
 #with log ↓          #no log ↓ 
 #nohup ./start.sh &  #nohup ./start.sh > /dev/null 2>&1 &
 #TODO start apache if in docker & start clashcheck with while if no false

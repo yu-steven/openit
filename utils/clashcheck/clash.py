18688f2eb4f2cb010ece5a49c4d8d4571d42f03e
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-12-01 16:22:11 +0800

    Update clash.py

--- utils/clashcheck/clash.py
+++ utils/clashcheck/clash.py
@@ -119,6 +119,8 @@ def filter(config):
                 x = list[i]
                 authentication = ''
                 x['port'] = int(x['port'])
+                if x['password'].isdigit():
+                    x['password'] = int(x['password'])
                 try:
                     ip = str(socket.gethostbyname(x["server"]))
                 except:

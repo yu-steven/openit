fc7b9778d0eb79aa80a132d52bbc3ec560d21585
Steven Yu <78414278+yu-steven@users.noreply.github.com>
2022-12-01 11:59:59 +0800

    Update clash.py

--- utils/clashcheck/clash.py
+++ utils/clashcheck/clash.py
@@ -132,13 +132,12 @@ def filter(config):
                         if x['cipher'] not in ss_supported_ciphers:
                             ss_omit_cipher_unsupported = ss_omit_cipher_unsupported + 1
                             continue
-                        if country != 'CN':
-                            if ip in iplist:
-                                ss_omit_ip_dupe = ss_omit_ip_dupe + 1
-                                continue
-                            else:
-                                iplist[ip] = []
-                                iplist[ip].append(x['port'])
+                        if ip in iplist:
+                            ss_omit_ip_dupe = ss_omit_ip_dupe + 1
+                            continue
+                        else:
+                            iplist[ip] = []
+                            iplist[ip].append(x['port'])
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SSS'
                         authentication = 'password'
                     except:
@@ -151,12 +150,11 @@ def filter(config):
                             continue
                         if x['protocol'] not in ssr_supported_protocol:
                             continue
-                        if country != 'CN':
-                            if ip in iplist:
-                                continue
-                            else:
-                                iplist.append(ip)
-                                iplist[ip].append(x['port'])
+                        if ip in iplist:
+                            continue
+                        else:
+                            iplist.append(ip)
+                            iplist[ip].append(x['port'])
                         authentication = 'password'
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'SSR'
                     except:
@@ -230,13 +228,10 @@ def filter(config):
                     continue
 
                 if ip in iplist and x['port'] in iplist[ip]:
-                    if country != 'CN':
+                    if x[authentication] in passlist:
                         continue
                     else:
-                        if x[authentication] in passlist:
-                            continue
-                        else:
-                            passlist.append(x[authentication])
+                        passlist.append(x[authentication])
                 else:
                     try:
                         iplist[ip].append(x['port'])
